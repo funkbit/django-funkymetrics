4ac09c2ebf91f0f6aef45bdeffe617374e08d2f9
Christian Mikalsen <christian@funkbit.no>
2013-02-26 11:33:48 +0100

    Support for passing in user objects directly instead of user to record_event.

--- README.md
+++ README.md
@@ -34,10 +34,14 @@ Simply import `record_event` in your code where you want to track events, and ca
     from funkymetrics.events import record_event
     
     # Without properties
-    record_event('downgraded')
+    record_event(request, 'downgraded')
     
     # With properties
-    record_event('upgraded plan', {'to_plan': 'Standard'})
+    record_event(request, 'upgraded plan', {'to_plan': 'Standard'})
+
+The request object is used to identify the user the event is associated with.
+
+When the request isn't available, a user instance can be passed instead. Note that we can't extract the anonymous KISSmetrics identifier from the user object, so it doesn't really make sense to pass anonymous users.
 
 ## Identifying users
 
--- funkymetrics/events.py
+++ funkymetrics/events.py
@@ -1,16 +1,31 @@
-from funkymetrics import tasks
+from django.contrib.auth.models import User
+
+from funkymetrics.tasks import send_event
 
 
-def record_event(request, action, props=None):
+def record_event(request_or_user, action, props=None):
     """
     Invokes a task to asynchronously record an event.
     """
 
-    # Get user identifier
-    if request.user.is_authenticated():
-        identity = request.user.username
+    identity = ''
+    user = request_or_user
+
+    # If argument isn't a user, assume it's a request with a user.
+    if not isinstance(request_or_user, User):
+        user = request_or_user.user
+
+    if user is not None and user.is_authenticated():
+
+        # Identify by username
+        identity = user.username
+
     else:
-        identity = request.COOKIES.get('km_ai', '')
+
+        # Attempt to identify by KISSmetrics ID from cookie
+        if hasattr(request_or_user, 'COOKIES', None) is not None:
+            identity = request_or_user.COOKIES.get('km_ai', '')
 
     # Queue task
-    tasks.send_event.delay(identity, action, props)
+    if identity != '':
+        send_event.delay(identity, action, props)

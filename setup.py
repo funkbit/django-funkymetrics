ee046973d2c8f8c17f8d8c4ee58ad444570d356e
Jon Lønne <jon@funkbit.no>
2013-06-06 16:13:42 +0200

    Cleaned up setup.py and license.

--- setup.py
+++ setup.py
@@ -13,16 +13,17 @@ if sys.argv[-1] == "publish":
     publish()
     sys.exit()
 
-setup(name='django-funkymetrics',
-      version=__version__,
-      description='Super simple Django application for easily tracking events and submitting them asynchronously to KISSmetrics.',
-      long_description=open('README.md').read(),
-      author='Funkbit AS',
-      author_email='post@funkbit.no',
-      url='https://github.com/funkbit/django-funkymetrics',
-      packages=['funkymetrics', ],
-      license='BSD',
-      classifiers=(
+setup(
+    name='django-funkymetrics',
+    version=__version__,
+    description='Super simple Django application for easily tracking events and submitting them asynchronously to KISSmetrics.',
+    long_description=open('README.md').read(),
+    author='Funkbit',
+    author_email='post@funkbit.no',
+    url='https://github.com/funkbit/django-funkymetrics',
+    packages=['funkymetrics', ],
+    license='BSD',
+    classifiers=(
         "Development Status :: 4 - Beta",
         "Framework :: Django",
         "Intended Audience :: Developers",
@@ -30,5 +31,5 @@ setup(name='django-funkymetrics',
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
-        )
-     )
+    )
+)
